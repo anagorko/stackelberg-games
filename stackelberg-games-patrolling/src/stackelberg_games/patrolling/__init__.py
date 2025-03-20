59d8c1e0b05e9eefda3267a9849d1f78a49e2a6b
Andrzej Nagórko <>
2025-03-20 06:25:00 +0100

    Fixed imports

--- stackelberg-games-patrolling/src/stackelberg_games/patrolling/__init__.py
+++ stackelberg-games-patrolling/src/stackelberg_games/patrolling/__init__.py
@@ -1,9 +1,10 @@
 from .bisect_solver import BisectSolver
 from .problem import patrolling_problem
-from .setting import john_et_al
+from .setting import graph_environment, john_et_al
 
 __all__ = [
     "BisectSolver",
+    "graph_environment",
     "john_et_al",
     "patrolling_problem"
 ]
