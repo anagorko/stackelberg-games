cac0aaaf04080cf59069ff6009b39c4fbcfb47f1
Andrzej Nagórko <>
2025-04-03 12:40:25 +0200

    Entrypoints for scripts, some fixes for gdynia_diagram

--- stackelberg-games-patrolling/scripts/uai_gdynia_diagram.py
+++ stackelberg-games-patrolling/scripts/uai_gdynia_diagram.py
@@ -2,6 +2,9 @@
 Generates some figures for UAI paper.
 """
 
+import os
+import warnings
+
 import cartopy.crs as ccrs
 from cartopy.io.img_tiles import GoogleTiles, Stamen, OSM
 import matplotlib.pyplot as plt
@@ -33,7 +36,7 @@ def main():
 
     fig = plt.figure(figsize=(12, 8), dpi=300)
     ax = fig.add_subplot(1, 1, 1, projection=web_mercator)
-    print(bounds)
+    print(f"Figure extent: {bounds}")
     ax.set_extent([bounds[1], bounds[3], bounds[0], bounds[2]], crs=ccrs.PlateCarree())
     ax.add_image(tiler, 16)
     # ax.gridlines(draw_labels=True)
@@ -42,7 +45,7 @@ def main():
         lat_u, lon_u = positions[u]
         lat_v, lon_v = positions[v]
 
-        print(f'Plotting line {lon_u, lat_u} -- {lon_v, lat_v}')
+        print(f'Plotting line ({lon_u:.6f}, {lat_u:.6f}) -- ({lon_v:.6f}, {lat_v:.6f})')
         ax.plot([lon_u, lon_v], [lat_u, lat_v], '--',
                 color='black', linewidth=1, alpha=0.5,
                 transform=ccrs.PlateCarree(), zorder=4)
@@ -51,12 +54,17 @@ def main():
     rect_h = 0.0005
     for node, position in positions.items():
         if node.startswith('B_'): # Docks - attack targets
+            print(f"Plotting square at ({position[1]:.6f}, {position[0]:.6f})")
             ax.add_patch(mpatches.Rectangle(xy=[position[1]-rect_w/2, position[0]-rect_h/2], width=rect_w, height=rect_h, facecolor='red', edgecolor='black', alpha=0.5, transform=ccrs.PlateCarree()))
         else:
-            ax.tissot(rad_km=0.02, lons=[position[1]], lats=[position[0]], alpha=0.5, edgecolor='black', zorder=5)
+            print(f"Plotting circle at ({position[1]:.6f}, {position[0]:.6f})")
+            ax.tissot(rad_km=0.02, lons=[position[1]], lats=[position[0]], transform=ccrs.PlateCarree(), alpha=0.5, edgecolor='black', zorder=5)
 
-    plt.savefig('output/gdynia_diagram.png', bbox_inches='tight')
-
+    os.makedirs(sgp.directories.plots_dir, exist_ok=True)
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", message="Approximating coordinate system.*")
+        plt.savefig(f'{sgp.directories.plots_dir}/gdynia_diagram.png', bbox_inches='tight')
+    print(f"\nSaved diagram to {f'{sgp.directories.plots_dir}/gdynia_diagram.png'}")
 
 if __name__ == '__main__':
     main()
