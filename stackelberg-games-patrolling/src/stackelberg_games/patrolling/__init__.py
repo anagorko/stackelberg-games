230971826436d1abaf5b8a24b29136aa6c28346f
Andrzej Nagórko <>
2025-04-05 16:09:02 +0200

    Do not install into scripts/ directory

--- stackelberg-games-patrolling/src/stackelberg_games/patrolling/__init__.py
+++ stackelberg-games-patrolling/src/stackelberg_games/patrolling/__init__.py
@@ -1,23 +1,28 @@
 from .bisect_solver import BisectSolver
 from . import directories
 from .mts import MapTileService
+from .plan import DefensePlan
 from .problem import PatrollingProblem, patrolling_problem
 from .problem_instances import gdynia_problem
 from .pwl import PWLMap
 from .schedule import Schedule
 from .setting import PatrollingEnvironment, Rational, basilico_et_al, gdynia_graph, graph_environment, john_et_al, port_gdynia
-from .space import ConcreteSpace, PathSpace, PatrollingSpace, TensorProductSpace, WeightedGraphSpace
+from .space import Action, ConcreteSpace, PathSpace, PatrollingSpace, State, StateActionSpace, TensorProductSpace, WeightedGraphSpace
 
 
 __all__ = [
+    "Action",
     "BisectSolver",
     "ConcreteSpace",
+    "DefensePlan",
     "PathSpace",
     "PatrollingEnvironment",
     "PatrollingProblem",
     "PatrollingSpace",
     "PWLMap",
     "Rational",
+    "State",
+    "StateActionSpace",
     "TensorProductSpace",
     "WeightedGraphSpace",
     "basilico_et_al",
